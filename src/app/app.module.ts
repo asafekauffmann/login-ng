bd5e3f1cdba0895717eed5f8022e809e0d7b7ae2
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-09-22 12:03:20 -0300

    up css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header/header.component';
 import { HomeComponent } from './view/home/home/home.component';
 import { DetailsComponent } from './view/details/details/details.component';
 import { LoginComponent } from './view/login/login.component';
+
 import { AuthService } from './view/login/auth.service';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
 
 @NgModule({
   declarations: [
@@ -20,7 +27,9 @@ import { AuthService } from './view/login/auth.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [AuthService ],
   bootstrap: [AppComponent]
