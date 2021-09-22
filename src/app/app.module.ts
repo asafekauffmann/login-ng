bdd90736131a5f6a81faab80ebebb6131437342b
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2021-09-22 11:17:40 -0300

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,6 +8,7 @@ import { HeaderComponent } from './components/header/header/header.component';
 import { HomeComponent } from './view/home/home/home.component';
 import { DetailsComponent } from './view/details/details/details.component';
 import { LoginComponent } from './view/login/login.component';
+import { AuthService } from './view/login/auth.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,7 @@ import { LoginComponent } from './view/login/login.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
